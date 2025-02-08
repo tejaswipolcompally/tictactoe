13d4ab343cc5e9faa840e66d42ffa44ff8c73268
tejaswipolcompally <tejaswi182005@gmail.com>
2025-02-08 20:11:38 +0530

    added vercel.json

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -67,7 +67,7 @@ app.post("/login", async (req, res) => {
 
         res.cookie("authToken", token, {
             httpOnly: true,
-            secure: false, // Set to true in production
+            secure: true, // Set to true in production
             sameSite: "Strict",
             maxAge: 3600000
         });
